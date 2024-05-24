5ba6ea2d94d8f9e072d23f20582a538e685c61c5
jamesLSI <james.currer@iuk.ukri.org>
2024-05-24 18:46:17 +0100

    Full res boundaries

--- pcc_boundaries.R
+++ pcc_boundaries.R
@@ -3,12 +3,14 @@ library(leaflet)
 library(htmltools)
 library(geojsonR)
 
+## get geojson from geoportal API - ultra generalised police force areas 
+pcc_regions_ultra_genrealised <- sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Police_Force_Areas_December_2023_EW_BUC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
 
-pcc_regions <- sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Police_Force_Areas_December_2023_EW_BUC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
+## get geojson from geoportal API - full resolution police force areas
+pcc_regions_full_resolution <- sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Police_Force_Areas_December_2023_EW_BFC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
 
-pcc_regions$PFA23NM
-
-pcc_regions %>% 
+## draw police force area boundaries using leaflet
+pcc_regions_full_resolution %>% 
   leaflet() %>% 
   setView(lng = -2.4, lat = 55.5, zoom = 6) %>%
   addProviderTiles(providers$OpenStreetMap.Mapnik,
