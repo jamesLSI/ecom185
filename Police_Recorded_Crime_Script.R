00bd1a13392f28b5d4a36aa75f8a4195c95c0a85
jamesLSI <james.currer@iuk.ukri.org>
2024-05-30 13:59:24 +0100

    modifed dat script

--- Police_Recorded_Crime_Script.R
+++ Police_Recorded_Crime_Script.R
@@ -7,30 +7,30 @@
 # library(readODS)
 # data <- read_ods("/Users/bradleyferris/Documents/EMAP/R/Project/Police Recorded Crime Data.ods")
 # view(data)
-start <- Sys.time()
-data_1213 <- read_ods("Police Recorded Crime Data.ods", sheet = "2012_13")
-data_1314 <- read_ods("Police Recorded Crime Data.ods", sheet = "2013_14")
-data_1415 <- read_ods("Police Recorded Crime Data.ods", sheet = "2014_15")
-data_1516 <- read_ods("Police Recorded Crime Data.ods", sheet = "2015_16")
-data_1617 <- read_ods("Police Recorded Crime Data.ods", sheet = "2016_17")
-data_1718 <- read_ods("Police Recorded Crime Data.ods", sheet = "2017_18")
-data_1819 <- read_ods("Police Recorded Crime Data.ods", sheet = "2018_19")
-data_1920 <- read_ods("Police Recorded Crime Data.ods", sheet = "2019_20")
-data_2021 <- read_ods("Police Recorded Crime Data.ods", sheet = "2020_21")
-data_2122 <- read_ods("Police Recorded Crime Data.ods", sheet = "2021_22")
-data_2223 <- read_ods("Police Recorded Crime Data.ods", sheet = "2022_23")
-data_2324 <- read_ods("Police Recorded Crime Data.ods", sheet = "2023_24")
-data <- rbind(data_1213,data_1314,data_1415,data_1516,data_1617,data_1718,data_1819,data_1920,data_2021,data_2122,data_2223,data_2324)
-end <- Sys.time()
-end-start
+# data_1213 <- read_ods("Police Recorded Crime Data.ods", sheet = "2012_13")
+# data_1314 <- read_ods("Police Recorded Crime Data.ods", sheet = "2013_14")
+# data_1415 <- read_ods("Police Recorded Crime Data.ods", sheet = "2014_15")
+# data_1516 <- read_ods("Police Recorded Crime Data.ods", sheet = "2015_16")
+# data_1617 <- read_ods("Police Recorded Crime Data.ods", sheet = "2016_17")
+# data_1718 <- read_ods("Police Recorded Crime Data.ods", sheet = "2017_18")
+# data_1819 <- read_ods("Police Recorded Crime Data.ods", sheet = "2018_19")
+# data_1920 <- read_ods("Police Recorded Crime Data.ods", sheet = "2019_20")
+# data_2021 <- read_ods("Police Recorded Crime Data.ods", sheet = "2020_21")
+# data_2122 <- read_ods("Police Recorded Crime Data.ods", sheet = "2021_22")
+# data_2223 <- read_ods("Police Recorded Crime Data.ods", sheet = "2022_23")
+# data_2324 <- read_ods("Police Recorded Crime Data.ods", sheet = "2023_24")
+# data <- rbind(data_1213,data_1314,data_1415,data_1516,data_1617,data_1718,data_1819,data_1920,data_2021,data_2122,data_2223,data_2324)
 # view(data)
-# Data contains British Transport Police, Action Fraud and potentially other things so we may want to remove anything beyond the 43 police forces in England and Wales
 
+
+
+# Data contains British Transport Police, Action Fraud and potentially other things so we may want to remove anything beyond the 43 police forces in England and Wales
 library(tidyverse)
 library(readODS)
-
+## get list of sheets in ods ####
 readODS::list_ods_sheets("Police Recorded Crime Data.ods")
 
+## read in all useful sheets (exclude first and second sheets) and combine into tidy format ####
 for (i in 3:14) {
   sheet <- read_ods("Police Recorded Crime Data.ods", sheet = i)
   if (exists("output") == T) {
